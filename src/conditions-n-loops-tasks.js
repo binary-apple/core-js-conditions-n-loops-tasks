18b05c443ceb7846a18488f1376db3d0106ca4c4
Anna Perevedentseva <anna.pereved@gmail.com>
2024-12-23 23:37:16 +0100

    feat: implement shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -464,8 +464,38 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let currentIteraction = 0;
+  let finalResult = str;
+  const shuffle = (string) => {
+    let result = '';
+    for (let i = 0; i < string.length; i += 2) {
+      result += string[i];
+    }
+    for (let i = 1; i < string.length; i += 2) {
+      result += string[i];
+    }
+    return result;
+  };
+  const areEqual = (str1, str2) => {
+    if (str1.length !== str2.length) {
+      return false;
+    }
+    for (let i = 0; i < str1.length; i += 1) {
+      if (str1[i] !== str2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  while (currentIteraction < iterations) {
+    finalResult = shuffle(finalResult);
+    currentIteraction += 1;
+    if (areEqual(str, finalResult)) {
+      currentIteraction = iterations - (iterations % currentIteraction);
+    }
+  }
+  return finalResult;
 }
 
 /**
